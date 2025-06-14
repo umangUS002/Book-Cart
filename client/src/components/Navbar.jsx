b74840499ebab5ab716b76a4a0e9d8fadd809a7f
amuojewellery <amuojewellery@gmail.com>
2025-06-14 19:07:08 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,12 +7,12 @@ function Navbar() {
     const {navigate, token} = useAppContext();
 
   return (
-    <div className='flex  justify-between items-center px-8 sm:px-20 xl:px-32 cursor pointer shadow max-sm:py-2'>
-        <div className='flex -ml-4'>
-            <img onClick={()=> navigate('/')} src={assets.logo} className='h-16 max-sm:h-10 max-sm:w-12 w-18 cursor-pointer mt-3 max-sm:mt-2.5 mr-2' />
-            <h1 onClick={()=> navigate('/')} className='mt-6 max-sm:mt-4 -ml-3 font-bold text-3xl max-sm:text-xl text-primary cursor-pointer'>Book Cart</h1>
+    <div className='flex  justify-between items-center px-2 sm:px-20 xl:px-32 cursor pointer shadow max-sm:py-2'>
+        <div className='flex -ml-4 max-sm:px-4'>
+            <img onClick={()=> navigate('/')} src={assets.logo} className='h-16 max-sm:h-10 max-sm:w-12 w-18 cursor-pointer mt-3 max-sm:mt-2 mr-2' />
+            <h1 onClick={()=> navigate('/')} className='mt-6 max-sm:mt-3 -ml-3 font-bold text-3xl max-sm:text-xl text-primary cursor-pointer'>Book Cart</h1>
         </div>
-      <button onClick={()=>navigate('/admin')} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-5 max-sm:px-3 max-sm:mt-2 sm:px-10 py-2.5'>
+      <button onClick={()=>navigate('/admin')} className='flex items-center gap-2 max-sm:gap-1 rounded-full text-sm max-sm:text-xs cursor-pointer bg-primary text-white px-5 max-sm:px-3 max-sm:mt-1 sm:px-10 py-2.5'>
         {token ? 'Dashboard' : 'Admin'}
         <img src={assets.arrow}  alt='arrow' />
       </button>
