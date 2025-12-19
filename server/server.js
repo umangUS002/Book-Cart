3b62ff218a92afef29b8331bdbca3215a288e179
Umang Srivastava <btech10271.23@bitmesra.ac.in_umangUS002>
2025-12-19 14:42:10 +0530

    one

--- server/server.js
+++ server/server.js
@@ -1,49 +1,41 @@
 import express from "express";
-import cors from "cors";
+import 'dotenv/config'
 import connectDB from "./configs/db.js";
 import bookRouter from "./routes/bookRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
+import cors from 'cors';
 import authRouter from "./routes/authRoutes.js";
 import wishlistRouter from "./routes/wishlistRoutes.js";
 import recRouter from "./routes/recommendationRoutes.js";
 
 const app = express();
 
-let isConnected = false;
-async function ensureDB() {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-}
+await connectDB();
 
-const allowedOrigins = process.env.CLIENT_ORIGIN
-  ? process.env.CLIENT_ORIGIN.split(",")
-  : [];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error("CORS blocked"));
-  },
-  credentials: true,
-}));
-
-app.options("*", cors());
+// Middlewares
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN, // React dev server
+    credentials: true,               // ALLOW cookies
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
-app.use(async (req, res, next) => {
-  await ensureDB();
-  next();
+//Routes
+app.get('/',(req,res)=> res.send("API is working"));
+app.use('/api/book', bookRouter);
+app.use('/api/admin', adminRouter);
+
+app.use('/api/auth', authRouter);
+app.use('/api/wishlist', wishlistRouter);
+app.use('/api/recommendations', recRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT)
 });
-
-app.get("/", (req, res) => res.send("API working"));
-
-app.use("/api/book", bookRouter);
-app.use("/api/admin", adminRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/wishlist", wishlistRouter);
-app.use("/api/recommendations", recRouter);
 
 export default app;
