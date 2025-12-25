287cbb3662005c11043580eadea1ee6dd5aa083f
Umang Srivastava <btech10271.23@bitmesra.ac.in_umangUS002>
2025-12-25 20:02:44 +0530

    modified server.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,5 @@
 import { Webhook } from "svix";
-import User from "../models/User";
+import User from "../models/User.js";
 
 // API Controller Function to Manage Clerk User with Database
 const clerkWebhooks = async(req,res) => {
